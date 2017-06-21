26777818d88bef9f3917bdcc8fe437c806058236
James Harris <wopian@wopian.me>
2017-06-21 15:58:27 +0100

    chore: improve error colours

--- tools/validate.js
+++ tools/validate.js
@@ -1,6 +1,6 @@
 const { readFileSync } = require('fs')
 const { validate } = require('drafter.js')
-const { gray, red } = require('chalk')
+const { bgRed, yellow, gray } = require('chalk')
 
 const file = readFileSync('apiary.apib', 'utf8')
 
@@ -10,15 +10,15 @@ validate(file, {
   if (err) console.log('\nBlueprint has errors!\n\n', err)
   if (res) {
     console.log('\nBlueprint has semantic issues!\n')
-    res.content.forEach(({ element, meta, attributes, content }) => {
-      console.log(`${meta.classes[0]}: ${content}\n`)
+    res.content.forEach(({ meta, attributes, content }) => {
+      console.log(`${bgRed(' ' + meta.classes[0].toUpperCase() + ' ')} ${content}\n`)
 
       attributes.sourceMap.forEach(asm => {
         asm.content.forEach(pos => {
           const before = file.toString().slice(pos[0] - 75, pos[0])
           const after = file.toString().slice(pos[0] + pos[1], pos[0] + pos[1] + 75)
           const highlight = file.toString().slice(pos[0], pos[0] + pos[1])
-          console.log(gray(before + red(highlight) + after) + '\n')
+          console.log(gray(before) + yellow(highlight) + gray(after) + '\n')
         })
 
       })
